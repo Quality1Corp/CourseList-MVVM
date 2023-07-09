faf7c1eb318b2ae9160e0ac982ca4bff46b1644b
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-07-09 17:17:02 +0300

    Initial Commit

---
+++ CourseList-MVVM/CourseList_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseList_MVVMApp.swift
+//  CourseList-MVVM
+//
+//  Created by MikhaiL on 09.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CourseList_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
